59cba172a21faea27238f1cd7877ecb4f999250c
Angelo Gabriel Albonetti <angeloalbonetti1@hotmail.com>
2026-05-28 20:40:33 -0300

    foi bb]

--- aep/vis.js
+++ aep/vis.js
@@ -1,7 +1,59 @@
 const de = JSON.parse(
-    localStorage.getItem("denuncia")
+    localStorage.getItem("de")
 );
 
-function mostar() {
 
+function linda() {
+    if (de) {
+        document.body.innerHTML = `
+
+        <div class="flex justify-center items-center min-h-screen bg-base-200">
+            <div class="card bg-base-100 w-96 shadow-2xl">
+
+                <figure>
+                    <p>
+                        <strong>Foto:</strong>
+                        ${de.foto}
+                    </p>
+                </figure>
+
+                <div class="card-body">
+
+                    <h2 class="card-title">
+                        Denúncia registrada
+                    </h2>
+
+                    <p>
+                        <strong>Descrição:</strong>
+                        ${de.descricao}
+                    </p>
+
+                    <p>
+                        <strong>Localização:</strong>
+                        ${de.localizacao}
+                    </p>
+
+                    <p>
+                        <strong>Data:</strong>
+                        ${de.data}
+                    </p>
+
+
+
+                    <div class="card-actions justify-end">
+
+                        <a class="btn btn-primary" href='ini.html'" >
+                            Voltar</a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+        `;
+    }
 }
+
+linda();
